377d09c9b07a42d884d23fc781134073fc75f767
Wesley Assis <88063740+WesleyAssiss@users.noreply.github.com>
2023-07-19 01:53:02 -0300

    ServidorSubtracao.py

--- SistemaOperacoesMatematicasDistribuida/ServidorSubtracao.py
+++ SistemaOperacoesMatematicasDistribuida/ServidorSubtracao.py
@@ -1,27 +1,27 @@
-import socket
+import socket  # Importa o módulo socket para trabalhar com comunicação em rede.
 
-HOST = 'localhost'
-PORTA = 60001
+HOST = 'localhost'  # Define o endereço do servidor como localhost (máquina local).
+PORTA = 60001  # Define a porta do servidor como 60001.
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORTA))
-s.listen(5)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Cria um novo socket TCP/IP.
+s.bind((HOST, PORTA))  # Associa o socket ao endereço e porta definidos.
+s.listen(5)  # Inicia o modo de escuta do servidor, permitindo até 5 conexões pendentes.
 
-print("Servidor de Subtração iniciado. Aguardando conexão de um cliente...")
+print("Servidor de Subtração iniciado. Aguardando conexão de um cliente...")  # Exibe uma mensagem informando o início do servidor.
 
-while True:
-    conn, endereco = s.accept()
-    print("Conectado ao Servidor de Subtração em:", endereco)
+while True:  # Loop infinito para sempre aguardar por novas conexões de clientes.
+    conn, endereco = s.accept()  # Aceita a conexão do cliente e obtém o endereço do cliente.
+    print("Conectado ao Servidor de Subtração em:", endereco)  # Exibe o endereço do cliente que se conectou.
 
-    while True:
-        mensagem = conn.recv(2048).decode()
+    while True:  # Loop infinito para receber múltiplas mensagens do cliente.
+        mensagem = conn.recv(2048).decode()  # Recebe a mensagem decodificada enviada pelo cliente.
 
-        if mensagem.lower() == 'sair':
-            conn.close()
-            print("Cliente desconectado do Servidor de Subtração:", endereco)
-            break
+        if mensagem.lower() == 'sair':  # Verifica se o cliente enviou a mensagem 'sair' para desconectar.
+            conn.close()  # Fecha a conexão com o cliente.
+            print("Cliente desconectado do Servidor de Subtração:", endereco)  # Exibe uma mensagem informando a desconexão.
+            break  # Sai do loop interno, encerrando a comunicação com esse cliente.
 
-        operandos = mensagem.split(',')[1:]
-        resultado = int(operandos[0]) - sum(int(op) for op in operandos[1:])
-        conn.send(str(resultado).encode())
-        break
+        operandos = mensagem.split(',')[1:]  # Separa os operandos da mensagem recebida, ignorando o primeiro elemento (que é a operação).
+        resultado = int(operandos[0]) - sum(int(op) for op in operandos[1:])  # Realiza a subtração dos operandos.
+        conn.send(str(resultado).encode())  # Envia o resultado da subtração de volta ao cliente em formato de string codificada em bytes.
+        break  # Sai do loop interno após enviar o resultado.
