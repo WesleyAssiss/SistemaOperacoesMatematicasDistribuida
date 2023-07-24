d62bf885e05380b564ea7995c896252f5ce3407b
Wesley Assis <88063740+WesleyAssiss@users.noreply.github.com>
2023-07-24 15:12:46 -0300

    ServidorDistribuido.py

--- SistemaOperacoesMatematicasDistribuida/ServidorDistribuido.py
+++ SistemaOperacoesMatematicasDistribuida/ServidorDistribuido.py
@@ -8,53 +8,61 @@ print("Aguardando conexão de clientes...")
 # Função para adicionar um novo cliente em uma thread separada.
 def novo_cliente():
     while True:
-        cliente, endereco = server.accept()  # Aceita a conexão de um novo cliente.
+        cliente, endereco = server.accept()  # Aceita a conexão do cliente.
         clientes.append(cliente)  # Adiciona o cliente à lista de clientes conectados.
-        print(f"Cliente conectado: {endereco}")  # Exibe o endereço do cliente recém-conectado.
-        threading.Thread(target=troca_mensagem, args=(cliente, endereco)).start()  # Inicia uma nova thread para lidar com as mensagens desse cliente.
+        print(f"Cliente conectado: {endereco}")
+        threading.Thread(target=troca_mensagem, args=(cliente, endereco)).start()  # Cria uma thread para cada cliente conectado.
 
 def troca_mensagem(cliente, endereco):
     while True:
-        mensagem_decodificada = cliente.recv(2048).decode()  # Recebe a mensagem do cliente e a decodifica.
+        mensagem_decodificada = cliente.recv(2048).decode()
         if mensagem_decodificada.lower() == 'sair':
-            cliente.send('Desconectando...'.encode())  # Envia uma mensagem de confirmação de desconexão para o cliente.
-            cliente.close()  # Fecha a conexão com o cliente.
-            clientes.remove(cliente)  # Remove o cliente da lista de clientes conectados.
-            print("Cliente desconectado:", endereco)  # Exibe o endereço do cliente que foi desconectado.
+            cliente.send('Desconectando...'.encode())
+            cliente.close()
+            clientes.remove(cliente)
+            print("Cliente desconectado:", endereco)
             break
-        elif mensagem_decodificada.split(',')[0] == '+':
-            # Se a operação for adição, chama a função para enviar a mensagem para o servidor de soma.
-            servidor_adicao(cliente, mensagem_decodificada)
-        elif mensagem_decodificada.split(',')[0] == '-':
-            # Se a operação for subtração, chama a função para enviar a mensagem para o servidor de subtração.
-            servidor_subtracao(cliente, mensagem_decodificada)
-        else:
-            cliente.send('Entrada não reconhecida.'.encode())  # Envia uma mensagem de erro para o cliente caso a operação seja inválida.
 
-def servidor_adicao(cliente, msg):
+
+        elif mensagem_decodificada.split(',')[0] == '+':
+
+            resultado = servidor_soma(mensagem_decodificada)
+            cliente.send(resultado.encode())
+
+
+        elif mensagem_decodificada.split(',')[0] == '-':
+
+            resultado = servidor_subtracao(mensagem_decodificada)
+            cliente.send(resultado.encode())
+        else:
+            cliente.send('Entrada não reconhecida.'.encode())  # Envia mensagem de erro para o cliente.
+
+# Função que envia a mensagem contendo a operação de adição para o ServidorSoma
+def servidor_soma(msg):
     HOST = 'localhost'
     PORTA = 60000
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((HOST, PORTA))  # Conecta-se ao servidor de soma.
-    client.send(msg.encode())  # Envia a mensagem de operação e valores para o servidor de soma.
-    mensagem_recebida = client.recv(2048).decode()  # Recebe o resultado da operação do servidor de soma.
-    cliente.send(mensagem_recebida.encode())  # Envia o resultado da operação de volta para o cliente.
-    client.close()  # Fecha a conexão com o servidor de soma.
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Cria o socket para comunicação com o ServidorSoma
+    client.connect((HOST, PORTA))  # Conecta ao ServidorSoma através do endereço e porta especificados
+    client.send(msg.encode())  # Envia a mensagem codificada para o ServidorSoma
+    mensagem_recebida = client.recv(2048).decode()  # Recebe a resposta do ServidorSoma e decodifica a mensagem
+    client.close()  # Fecha a conexão com o ServidorSoma
+    return mensagem_recebida  # Retorna o resultado da operação de adição.
 
-def servidor_subtracao(cliente, msg):
+# Função que envia a mensagem contendo a operação de subtração para o ServidorSubtracao
+def servidor_subtracao(msg):
     HOST = 'localhost'
     PORTA = 60001
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((HOST, PORTA))  # Conecta-se ao servidor de subtração.
-    client.send(msg.encode())  # Envia a mensagem de operação e valores para o servidor de subtração.
-    mensagem_recebida = client.recv(2048).decode()  # Recebe o resultado da operação do servidor de subtração.
-    cliente.send(mensagem_recebida.encode())  # Envia o resultado da operação de volta para o cliente.
-    client.close()  # Fecha a conexão com o servidor de subtração.
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Cria o socket para comunicação com o ServidorSubtracao
+    client.connect((HOST, PORTA))  # Conecta ao ServidorSubtracao através do endereço e porta especificados
+    client.send(msg.encode())  # Envia a mensagem codificada para o ServidorSubtracao
+    mensagem_recebida = client.recv(2048).decode()  # Recebe a resposta do ServidorSubtracao e decodifica a mensagem
+    client.close()  # Fecha a conexão com o ServidorSubtracao
+    return mensagem_recebida  # Retorna o resultado da operação de subtração.
 
 if __name__ == '__main__':
     HOST = 'localhost'
     PORTA = 50000
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((HOST, PORTA))  # Vincula o servidor a um endereço e porta específicos.
-    server.listen(5)  # Inicia a escuta do servidor por novas conexões.
-    threading.Thread(target=novo_cliente).start()  # Inicia uma nova thread para aceitar conexões de clientes.
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Cria o socket para o ServidorDistribuido
+    server.bind((HOST, PORTA))  # Vincula o servidor ao endereço e porta especificados
+    server.listen(5)  # Define o número máximo de conexões em espera
+    threading.Thread(target=novo_cliente).start()  # Inicia a thread para aceitar novos clientes e iniciar a função novo_cliente em uma thread separada.
