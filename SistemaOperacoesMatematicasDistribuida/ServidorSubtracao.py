b29315385eaae1d659f8e87efd29e8bf305d085e
Wesley Assis <88063740+WesleyAssiss@users.noreply.github.com>
2023-07-24 15:13:34 -0300

    ServidorSubtracao.py

--- SistemaOperacoesMatematicasDistribuida/ServidorSubtracao.py
+++ SistemaOperacoesMatematicasDistribuida/ServidorSubtracao.py
@@ -4,27 +4,25 @@ HOST = 'localhost'
 PORTA = 60001
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORTA))  # Vincula o servidor de subtração a um endereço e porta específicos.
-s.listen(5)  # Inicia a escuta do servidor de subtração por novas conexões.
+s.bind((HOST, PORTA))  # Vincula o servidor ao endereço e porta especificados.
+s.listen(5)  # Define o número máximo de conexões em espera.
 
 print("Servidor de Subtração iniciado. Aguardando conexão de um cliente...")
 
 while True:
-    conn, endereco = s.accept()  # Aceita a conexão de um novo cliente.
-    print("Conectado ao Servidor de Subtração em:", endereco)  # Exibe o endereço do cliente recém-conectado.
+    conn, endereco = s.accept()  # Aceita a conexão do cliente.
+    print("Conectado ao Servidor de Subtração em:", endereco)
 
     while True:
-        mensagem = conn.recv(2048).decode()  # Recebe a mensagem do cliente e a decodifica.
+        mensagem = conn.recv(2048).decode()  # Recebe a mensagem do cliente.
 
         if mensagem.lower() == 'sair':
             conn.close()  # Fecha a conexão com o cliente.
-            print("Cliente desconectado do Servidor de Subtração:", endereco)  # Exibe o endereço do cliente que foi desconectado.
+            print("Cliente desconectado do Servidor de Subtração:", endereco)
             break
 
-        operandos = mensagem.split(',')[1:]  # Separa os operandos da mensagem.
-        resultado = int(operandos[0]) - sum(int(op) for op in operandos[1:])  # Realiza a operação de subtração com os operandos.
-        conn.send(str(resultado).encode())  # Envia o resultado da subtração de volta para o cliente.
-        break  # Encerra o loop de tratamento de mensagens (considerando que a mensagem enviada pelo cliente contém apenas a operação de subtração).
+        operandos = mensagem.split(',')[1:]
+        resultado = int(operandos[0]) - sum(int(op) for op in operandos[1:])
 
-# Nota: O servidor de subtração também lida com apenas uma única mensagem de cada cliente antes de encerrar a conexão com ele.
-# Se fosse desejado que o servidor de subtração continuasse recebendo mais mensagens do mesmo cliente, seria necessário ajustar a lógica do loop aqui.
+        conn.send(str(resultado).encode())
+        break
