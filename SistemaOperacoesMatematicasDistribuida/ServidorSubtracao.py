a6bcbf4ba9d1f94b2aeadfcc81becd8a89ae3599
Wesley Assis <88063740+WesleyAssiss@users.noreply.github.com>
2023-07-24 15:37:30 -0300

    ServidorSubtracao.py

--- SistemaOperacoesMatematicasDistribuida/ServidorSubtracao.py
+++ SistemaOperacoesMatematicasDistribuida/ServidorSubtracao.py
@@ -1,28 +1,28 @@
 import socket
 
-HOST = 'localhost'
-PORTA = 60001
+HOST = 'localhost'  # Endereço IP do servidor.
+PORTA = 60001  # Número da porta que o servidor utilizará para aceitar conexões.
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Criação do socket.
 s.bind((HOST, PORTA))  # Vincula o servidor ao endereço e porta especificados.
 s.listen(5)  # Define o número máximo de conexões em espera.
 
 print("Servidor de Subtração iniciado. Aguardando conexão de um cliente...")
 
 while True:
-    conn, endereco = s.accept()  # Aceita a conexão do cliente.
+    conn, endereco = s.accept()  # Aceita a conexão do cliente e obtém o endereço do cliente.
     print("Conectado ao Servidor de Subtração em:", endereco)
 
     while True:
-        mensagem = conn.recv(2048).decode()  # Recebe a mensagem do cliente.
+        mensagem = conn.recv(2048).decode()  # Recebe a mensagem do servidor distribuido e a decodifica de bytes para string.
 
         if mensagem.lower() == 'sair':
             conn.close()  # Fecha a conexão com o cliente.
             print("Cliente desconectado do Servidor de Subtração:", endereco)
             break
 
-        operandos = mensagem.split(',')[1:]
-        resultado = int(operandos[0]) - sum(int(op) for op in operandos[1:])
+        operandos = mensagem.split(',')[1:]  # Separa os operandos da mensagem.
+        resultado = int(operandos[0]) - sum(int(op) for op in operandos[1:])  # Realiza a operação de subtração.
 
-        conn.send(str(resultado).encode())
-        break
+        conn.send(str(resultado).encode())  # Envia o resultado da subtração de volta para o servidor distribuido.
+        break  # Encerra a comunicação após enviar o resultado.
